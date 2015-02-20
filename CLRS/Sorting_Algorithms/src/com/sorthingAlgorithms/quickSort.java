b46f0c004ace75ccc4a5b060bf61e0cd5be8ebfd
Robert Mulholand <robmulholand@gmail.com>
2015-02-20 14:24:52 -0600

    add randomized partition to quicksort

--- CLRS/Sorting_Algorithms/src/com/sorthingAlgorithms/quickSort.java
+++ CLRS/Sorting_Algorithms/src/com/sorthingAlgorithms/quickSort.java
@@ -1,5 +1,7 @@
 package com.sorthingAlgorithms;
 
+import java.util.Random;
+
 public class quickSort implements sortable {
     @Override
     public Comparable[] mySort(Comparable[] list) {
@@ -9,10 +11,17 @@ public class quickSort implements sortable {
 
     public void quickSort(Comparable[] list, int beginning, int end) {
         if(beginning < end) {
-            int pivot = partition(list, beginning, end);
+            int pivot = randomPartition(list, beginning, end);
             quickSort(list, beginning, pivot - 1);
             quickSort(list, pivot + 1, end);
         }
+    }
+
+    public int randomPartition(Comparable[] list, int beginning, int end) {
+        Random random = new Random();
+        int randomPivot = random.nextInt(end - beginning) + beginning;
+        exchange(list, end, randomPivot);
+        return partition(list, beginning, end);
     }
 
     public int partition(Comparable[] list, int beginning, int end) {
